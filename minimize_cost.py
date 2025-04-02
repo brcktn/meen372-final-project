6f7d83a4a28a779abc5dee01ef6327631b3db43b
brcktn <29804204+brcktn@users.noreply.github.com>
2025-04-02 14:42:52 -0600

    moved constants, added varable list to minimize

--- minimize_cost.py
+++ minimize_cost.py
@@ -1,5 +1,16 @@
 from model import *
-from scipy.optimize import minimize
+from scipy.optimize import minimize 
+
+"""
+x[0]: length_diagonal
+x[1]: cross_section_height
+x[2]: cross_section_width
+x[3]: material_thickness
+x[4]: crossbar_diameter
+x[5]: hole_offset
+x[6]: start_height
+"""
+
 
 def obj():
     pass
