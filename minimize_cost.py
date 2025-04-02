27f70400d105043038e5a3b6b3a8fdcfbe77ed00
bman7500 <physicsbrig@gmail.com>
2025-04-02 14:35:30 -0600

    Added placeholder functions in minimize_cost

--- minimize_cost.py
+++ minimize_cost.py
@@ -1,3 +1,15 @@
 from model import *
 from scipy.optimize import minimize
 
+def obj():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    cons = []
+    x0 = []
+    bounds = []
+    
+    minimize(obj, x0, bounds=bounds, constraints=cons)
