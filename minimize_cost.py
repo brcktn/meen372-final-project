91c3ab7046816415d5dad228c4f5941f13977ba5
brcktn <29804204+brcktn@users.noreply.github.com>
2025-04-02 15:01:24 -0600

    added input docstring

--- minimize_cost.py
+++ minimize_cost.py
@@ -1,7 +1,16 @@
 from model import *
 from scipy.optimize import minimize 
 
-    
+"""
+x[0]: length_diagonal (float, inches)
+x[1]: cross_section_height (float, inches)
+x[2]: cross_section_width (float, inches)
+x[3]: material_thickness (float, inches)
+x[4]: crossbar_diameter (float, inches)
+x[5]: hole_offset (float, inches)
+x[6]: start_height (float, inches)
+"""
+
 def obj(x):
     return calc_cost(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],[9],[10])
 
