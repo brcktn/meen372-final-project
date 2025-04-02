79ad5b01d9831438a81161524ea4c26e0761995a
bman7500 <physicsbrig@gmail.com>
2025-04-02 14:55:07 -0600

    updated con1 funciton

--- minimize_cost.py
+++ minimize_cost.py
@@ -1,26 +1,21 @@
 from model import *
 from scipy.optimize import minimize 
 
-"""
-x[0]: length_diagonal
-x[1]: cross_section_height
-x[2]: cross_section_width
-x[3]: material_thickness
-x[4]: crossbar_diameter
-x[5]: hole_offset
-x[6]: start_height
-"""
+    
+def obj(x):
+    return calc_cost(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],[9],[10])
 
 
-def obj():
-    pass
-
+def con1(x):
+    P_cr = calc_critical_buckling_load()
+    n_buckling = P_cr / F_d
 
 
 
 if __name__ == '__main__':
+    
     cons = []
     x0 = []
     bounds = []
-    
-    minimize(obj, x0, bounds=bounds, constraints=cons)
+
+    minimize(calc_cost, x0, bounds=bounds, constraints=cons)
