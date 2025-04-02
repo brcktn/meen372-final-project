ad76b4013891937f87f0c8923e2900f1f36fe675
bman7500 <physicsbrig@gmail.com>
2025-04-02 15:00:51 -0600

    added con placeholders

--- minimize_cost.py
+++ minimize_cost.py
@@ -6,13 +6,19 @@ def obj(x):
     return calc_cost(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],[9],[10])
 
 
-def con1(x):
+def con1(x): #buckling constraint
     P_cr = calc_critical_buckling_load()
     F_d = calc_diagonal_force()
     n_buckling = P_cr / F_d
     return n_buckling - 3
 
-
+def con2(x): #tensile contraint
+    return None
+def con3(x): #tearout constrait
+    return None
+def con4(x):
+    return None
+    
 
 if __name__ == '__main__':
     
