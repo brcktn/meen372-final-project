b62284ea3df76327d3bb588d1010cc51553defa9
bman7500 <physicsbrig@gmail.com>
2025-04-03 11:49:00 -0600

    added parenthesis to angle calculation

--- minimize_cost.py
+++ minimize_cost.py
@@ -102,7 +102,7 @@ def con1(x):  # n_buckling 1
     )
     P_cr = 1.2 * pi**2 * E * I_xx / l**2
 
-    start_angle = degrees(arcsin(STARTING_HEIGHT / 2 / x[0]))
+    start_angle = degrees(arcsin((STARTING_HEIGHT / 2) / x[0]))
 
     F_d = calc_diagonal_force(FORCE, start_angle)
     n_buckling = P_cr / F_d
@@ -118,7 +118,7 @@ def con2(x):  # n_buckling 2
     I_yy = h * w**3 / 12 - 2 * h * (-t + w / 2) ** 3 / 3 + 2 * t * (-t + w / 2) ** 3 / 3
     P_cr = 1.2 * pi**2 * E * I_yy / l**2
 
-    start_angle = degrees(arcsin(STARTING_HEIGHT / 2 / x[0]))
+    start_angle = degrees(arcsin((STARTING_HEIGHT / 2) / x[0]))
 
     F_d = calc_diagonal_force(FORCE, start_angle)
     n_buckling = P_cr / F_d
@@ -127,7 +127,7 @@ def con2(x):  # n_buckling 2
 
 def con3(x):  # n_tensile
     S_y_cb = material_dict["steel 1030 1000C"]["S_y"]
-    start_angle = degrees(arcsin(STARTING_HEIGHT / 2 / x[0]))
+    start_angle = degrees(arcsin((STARTING_HEIGHT / 2) / x[0]))
     F_cb = calc_diagonal_force(FORCE, start_angle)
     n_tensile = S_y_cb / calc_crossbar_stress(F_cb, x[4])
 
@@ -135,7 +135,7 @@ def con3(x):  # n_tensile
 
 
 def con4(x): # n_tearout
-    start_angle = degrees(arcsin(STARTING_HEIGHT / 2 / x[0]))
+    start_angle = degrees(arcsin((STARTING_HEIGHT / 2) / x[0]))
     F_d = calc_diagonal_force(FORCE, start_angle)
     sigma_tearout = calc_tearout_stress(x[5], x[3], F_d)
     n_tearout = S_y / sigma_tearout
@@ -144,7 +144,7 @@ def con4(x): # n_tearout
 
 
 def con5(x): # n_bearing
-    start_angle = degrees(arcsin(STARTING_HEIGHT / 2 / x[0]))
+    start_angle = degrees(arcsin((STARTING_HEIGHT / 2) / x[0]))
     F_d = calc_diagonal_force(FORCE, start_angle)
     sigma_bearing = calc_bearing_stress(HOLE_DIAMETER, x[3], F_d)
     
@@ -153,7 +153,7 @@ def con5(x): # n_bearing
 
 
 def con6(x): # n_axial
-    start_angle = degrees(arcsin(STARTING_HEIGHT / 2 / x[0]))
+    start_angle = degrees(arcsin((STARTING_HEIGHT / 2) / x[0]))
     F_d = calc_diagonal_force(FORCE, start_angle)
     sigma_axial = calc_diagonal_axial_stress(HOLE_DIAMETER, x[3], x[1], F_d)
 
@@ -171,7 +171,7 @@ constraints = [
 ]
 
 bounds = [
-    (sqrt((STARTING_HEIGHT / 2) ** 2), 20),  # length_diagonal
+    ((STARTING_HEIGHT), 20),  # length_diagonal
     (0.25, 5),  # cross_section_height
     (0.25, 5),  # cross_section_width
     (0.01, 1),  # material_thickness
